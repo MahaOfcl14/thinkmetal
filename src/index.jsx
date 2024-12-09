d7883d699730717f53f0736449a8759fd90418b5
MahaOfcl14 <itzmahaarun@gmail.com>
2024-12-09 20:18:32 +0530

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import "./style.css"
+const HeadingElement = <h1 className="bg-red-500">Vite</h1>
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(HeadingElement,root)
