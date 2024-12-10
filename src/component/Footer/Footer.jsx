19aac3e923ba3cfbe59e14e0ad20c22688489755
MahaOfcl14 <itzmahaarun@gmail.com>
2024-12-10 22:48:32 +0530

    Add files via upload

---
+++ src/component/Footer/Footer.jsx
@@ -0,0 +1,60 @@
+const Footer = () => {
+    return (
+        <div>
+        <div class="2xl:container mx-auto ">
+
+            <div class="">
+
+                <div class="w-[90%] md:w-[80%] mx-auto grid sm:grid-cols-1 md:grid-cols-5">
+<div>
+<img src="https://ik.imagekit.io/ajql7av7t/Thinkmetal/Auto%20Layout%20Horizontal%20(3).png"
+    alt="Centered Image" className="pb-10"/>
+</div>
+
+<div className="">
+    <p className="font-[Inter] font-semibold pb-4">Product</p>
+    <p className="font-[Inter] font-light text-[#5F6980] hover:text-[#FD4C27] pb-3">Features</p>
+    <p className="font-[Inter] font-light text-[#5F6980] hover:text-[#FD4C27] pb-3">Pricing</p>
+    <p className="font-[Inter] font-light text-[#5F6980] hover:text-[#FD4C27] pb-3">Changelog</p>
+    <p className="font-[Inter] font-light text-[#5F6980] hover:text-[#FD4C27] pb-3">Support</p>
+
+
+                        
+                       
+
+</div>
+
+<div className="">
+    <p className="font-[Inter] font-semibold pb-4">Legal</p>
+    <p className="font-[Inter] font-light text-[#5F6980] hover:text-[#FD4C27] pb-3">Terms of Privacy</p>
+    <p className="font-[Inter] font-light text-[#5F6980] hover:text-[#FD4C27] pb-3">Privacy Policy</p>
+    <p className="font-[Inter] font-light text-[#5F6980] hover:text-[#FD4C27] pb-3">Security</p>
+</div>
+<div className="">
+    <p className="font-[Inter] font-semibold pb-4">Company</p>
+    <p className="font-[Inter] font-light text-[#5F6980] hover:text-[#FD4C27] pb-3">Blog</p>
+    <p className="font-[Inter] font-light text-[#5F6980] hover:text-[#FD4C27] pb-3">Contact</p>
+</div>
+
+<div className="">
+    <p className="font-[Inter] font-semibold pb-4">Social</p>
+    <p className="font-[Inter] font-light text-[#5F6980] hover:text-[#FD4C27] pb-3">Dribble</p>
+    <p className="font-[Inter] font-light text-[#5F6980] hover:text-[#FD4C27] pb-3">Behance</p>
+    <p className="font-[Inter] font-light text-[#5F6980] hover:text-[#FD4C27] pb-3">Discord</p>
+
+</div>
+
+
+
+
+
+                </div>
+                <p className="font-[Inter] pt-5  font-light text-center text-[#5F6980] hover:text-[#FD4C27] pb-16">All rights reserved.&copy; 2024 ThinkMetal</p>
+
+                </div>
+             
+                </div>
+                </div>
+    )
+}
+export default Footer
