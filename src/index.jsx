ddbc2490499ca247bd3bfcfc2a4ab510caaab269
MahaOfcl14 <itzmahaarun@gmail.com>
2024-12-10 22:44:21 +0530

    Add files via upload

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,36 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import ReactDOM from "react-dom/client"
+import Header from "./component/Header/Header"
+import Home from "./component/Home/Home"
+import Logo from "./component/Logo/Logo"
+import Share from "./component/Share/Share"
+import Feedback from "./component/Feedback/Feedback"
 import "./style.css"
-const HeadingElement = <h1 className="bg-red-500">Vite</h1>
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(HeadingElement,root)
+import Besttime from "./component/Besttime/Besttime"
+import Progress from "./component/Progress/Progress"
+import Review from "./component/Review/Review"
+import Designers from "./component/Designers/Designers"
+import Loop from "./component/Loop/Loop"
+import RequestDemo from "./component/RequestDemo/RequestDemo"
+import Footer from "./component/Footer/Footer"
+
+const AppLayout = () => {
+    return (
+        <div>
+            <Header />
+            <Home />
+            <Logo />
+            <Share />
+            <Feedback />
+            <Besttime />
+            <Progress />
+            <Review />
+            <Designers />
+            <Loop />
+            <RequestDemo />
+            <Footer />
+ </div>
+    )
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
